042af07d6c3cfc590063921011969b786d3f7725
VanaIgr <vanaigranov@gmail.com>
2023-08-02 23:54:42 -0500

    add animation for pressing 'copy' output button

--- elements.js
+++ elements.js
@@ -48,11 +48,16 @@ function createOutputElement() {
                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
             </svg>
         </div>
-        <div title="Скопировать изображенее" class="main-action-img">
-            <svg viewBox="0 0 24 24" style="fill: #f8f8f8" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M21 8C21 6.34315 19.6569 5 18 5H10C8.34315 5 7 6.34315 7 8V20C7 21.6569 8.34315 23 10 23H18C19.6569 23 21 21.6569 21 20V8ZM19 8C19 7.44772 18.5523 7 18 7H10C9.44772 7 9 7.44772 9 8V20C9 20.5523 9.44772 21 10 21H18C18.5523 21 19 20.5523 19 20V8Z"></path> 
-                    <path d="M6 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H6C4.34315 1 3 2.34315 3 4V18C3 18.5523 3.44772 19 4 19C4.55228 19 5 18.5523 5 18V4C5 3.44772 5.44772 3 6 3Z"></path> 
-            </svg>
+        <div class="main-action-cont">
+            <div title="Скопировать изображенее" class="main-action-img">
+                <svg viewBox="0 0 24 24" style="fill: #f8f8f8" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M21 8C21 6.34315 19.6569 5 18 5H10C8.34315 5 7 6.34315 7 8V20C7 21.6569 8.34315 23 10 23H18C19.6569 23 21 21.6569 21 20V8ZM19 8C19 7.44772 18.5523 7 18 7H10C9.44772 7 9 7.44772 9 8V20C9 20.5523 9.44772 21 10 21H18C18.5523 21 19 20.5523 19 20V8Z"></path> 
+                        <path d="M6 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H6C4.34315 1 3 2.34315 3 4V18C3 18.5523 3.44772 19 4 19C4.55228 19 5 18.5523 5 18V4C5 3.44772 5.44772 3 6 3Z"></path> 
+                </svg>
+            </div>
+            <div class="anim-background">
+                <div></div>
+            </div>
         </div>
         <div class="out-footer out-icons">
             <svg class="view-pdf" xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 2100 2100" stroke-width="150"><title>Открыть как PDF</title><path d="M1251.654 0c44.499 0 88.207 18.07 119.718 49.581l329.223 329.224c31.963 31.962 49.581 74.54 49.581 119.717V1920H169V0Zm-66.183 112.941H281.94V1807.06h1355.294V564.706H1185.47V112.94Zm112.94 23.379v315.445h315.445L1298.412 136.32Z"></path> <path d="M900.497 677.67c26.767 0 50.372 12.65 67.991 37.835 41.901 59.068 38.965 121.976 23.492 206.682-5.308 29.14.113 58.617 16.263 83.125 22.814 34.786 55.68 82.673 87.981 123.219 23.718 29.93 60.198 45.854 97.13 40.885 23.718-3.276 52.292-5.986 81.656-5.986 131.012 0 121.186 46.757 133.045 89.675 6.55 25.976 3.275 48.678-10.165 65.506-16.715 22.701-51.162 34.447-101.534 34.447-55.793 0-74.202-9.487-122.767-24.96-27.445-8.81-55.906-10.617-83.69-3.275-55.453 14.456-146.936 36.48-223.284 46.983-40.772 5.647-77.816 26.654-102.438 60.875-55.454 76.8-106.842 148.518-188.273 148.518-21.007 0-40.32-7.567-56.244-22.701-23.492-23.492-33.544-49.581-28.574-79.85 13.778-92.95 128.075-144.79 196.066-182.625 16.037-8.923 28.687-22.589 36.592-39.53l107.86-233.223c7.68-16.377 10.051-34.56 7.228-52.518-12.537-79.059-31.06-211.99 18.748-272.075 10.955-13.44 26.09-21.007 42.917-21.007Zm20.556 339.953c-43.257 126.607-119.718 264.282-129.996 280.32 92.273-43.37 275.916-65.28 275.916-65.28-92.386-88.998-145.92-215.04-145.92-215.04Z"></path></svg>
@@ -108,6 +113,7 @@ function createOutputElement() {
         image: el.querySelector('img'),
         del: el.querySelector('.delete'),
         mainActionImg: el.querySelector('.main-action-img'),
+        mainActionAnim: el.querySelector('.anim-background'),
         viewPdf: el.querySelector('.view-pdf'),
         viewImg: el.querySelector('.view-img'),
         downloadImg: el.querySelector('.download-img'),
@@ -242,6 +248,16 @@ function createAndInitOutputElement(scheme, schedule, doc, preview, parentElemen
                 new ClipboardItem(obj)
             ])
             console.log('done')
+            //https://stackoverflow.com/a/45036752/18704284
+            const el = element.mainActionAnim
+            const ch = el.firstElementChild 
+            el.setAttribute('data-anim', '')
+            el.style.animation = 'none'
+            ch.style.animation = 'none'
+            el.offsetHeight; //do I need this one?
+            ch.offsetHeight;
+            ch.style.animation = null
+            el.style.animation = null
         } catch (error) {
             console.error(error);
         }
@@ -315,16 +331,15 @@ const css = `
 }
 
 .main-action-img {
-    flex: 1 0 auto;
-
+    width: 100%; height: 100%;
     display: flex; 
     justify-content: center; 
     align-items: center;
 
-    background: #ffffff00;
-    transition: background 200ms;
-    
     & > * {
+        position: relative;
+        z-index: 1;
+
         min-width: 3rem;
         min-height: 3rem;
         max-width: max(30%, 3rem);
@@ -333,9 +348,17 @@ const css = `
 }
 
 
+@keyframes opacity-inc1 {
+    from { opacity: 0.5 }
+    to { opacity: 1 }
+}
+@keyframes opacity-inc2 {
+    from { opacity: 1 }
+    to { opacity: 0.5 }
+}
+
 @media (pointer: fine) {
     .out-overlay { background: #00000080; }
-    .main-action-img:hover { background: #ffffff50; }
 
     .out-overlay {
         opacity: 0;
@@ -344,6 +367,47 @@ const css = `
 
     .out-overlay:hover, .output[data-popup-opened=true] .out-overlay { 
         opacity: 1;
+    }
+
+    .main-action-cont {
+        position: relative;
+        flex: 1 0 auto;
+
+        &:hover > .anim-background > * {
+            background: #ffffff80;
+        }
+
+        & > .anim-background {
+            position: absolute; 
+            left:0;top:0;right:0;bottom:0;
+            z-index: 0;
+            pointer-events: none;
+
+            opacity: 1;
+            & > * {
+                width: 100%; height: 100%; 
+
+                opacity: 0.5;
+
+                background: #ffffff00;
+                transition: background 200ms;
+            }
+
+            &[data-anim] {
+                animation: opacity-inc2;
+                animation-duration: 0.4s;
+                animation-timing-function: ease-out;
+                animation-delay: 0.08s;
+                animation-fill-mode: both;
+
+                & > * { 
+                    animation: opacity-inc1; 
+                    animation-duration: 0.08s;
+                    animation-timing-function: cubic-bezier(1, 0, 1, 1);
+                    animation-fill-mode: both;
+                }
+            }
+        }
     }
 }
 
